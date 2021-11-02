6305e82e073e9a6924048885c33102e6c76ffbb6
Ahmed Mahrous <ahmedmahros425@gmail.com>
2021-11-02 21:51:12 +0200

    Initial Commit

---
+++ Tips Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tips Calculator
+//
+//  Created by Ma7rous on 11/2/21.
+//  Copyright © 2021 Ma7rous. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
